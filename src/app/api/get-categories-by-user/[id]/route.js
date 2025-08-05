3d9d5e1a0c7732972044f1cd86755e65da5be7c4
Samiul Hasan Siam <156831178+samiulhasansiam556@users.noreply.github.com>
2025-08-05 20:40:38 +0600

    fix bug for find-user feature

--- src/app/api/get-categories-by-user/[id]/route.js
+++ src/app/api/get-categories-by-user/[id]/route.js
@@ -1,11 +1,12 @@
 import Category from "@/models/categoryModel";
+import User from "@/models/userModel";
 import { middleware } from "../../../middleware/protectedRoute";
 import { NextResponse } from "next/server";
 import DBConnection from "@/lib/Db";
 
 export async function GET(req,{ params }) {
   const { id } = await params;
-  //console.log("id",id)
+  //console.log("idddd",id)
   if (!id) {
     return NextResponse.json({
       status: 400,
@@ -15,17 +16,29 @@ export async function GET(req,{ params }) {
 
   await DBConnection(); // Ensure the database connection is established
   try {
-    // console.log(req)
     const decode = middleware(req); // Middleware to check authentication
-
+    
     if (decode instanceof NextResponse) {
-      return decode; // If middleware returns a response, return it
+        return decode; // If middleware returns a response, return it
     }
 
-    const categories = await Category.find({ userid: id }).populate(
+
+    const user = await User.findOne({username: id});
+
+    if (!user) {
+      return NextResponse.json({
+        status: 404,
+        message: "User not found",
+      });
+    }
+
+    const userid = user._id;
+    //console.log("User ID:", userid);
+
+    const categories = await Category.find({ userid: userid }).populate(
       "blogid"
     ); // Populate blogid field and check userid
-    console.log("ccc", categories);
+    //console.log("ccc", categories);
     if (!categories || categories.length === 0) {
       return NextResponse.json({ status: 404, message: "No categories found" });
     }
